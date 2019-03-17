97ba9767ac98764dc67c864805c7595d820874da
king <>
2019-03-17 16:50:11 +0800

    增加仿微信图片压缩

--- iOSDemo/Category/UIImage+Extension.h
+++ iOSDemo/Category/UIImage+Extension.h
@@ -9,8 +9,33 @@
 #import <UIKit/UIKit.h>
 
 @interface UIImage (Extension)
+/**
+ *  争对ios7以上的系统适配新的图片资源
+ *
+ *  @param imageName 图片名称
+ *
+ *  @return 新的图片
+ */
 + (UIImage *) imageWithName:(NSString *) imageName;
+
 + (UIImage *) resizableImageWithName:(NSString *)imageName;
+/**
+ *  实现图片的缩小或者放大
+ *
+ *  @param image 原图
+ *  @param size  大小范围
+ *
+ *  @return 新的图片
+ */
 - (UIImage*) scaleImageWithSize:(CGSize)size;
 
+/**
+ *  仿微信对图片进行压缩
+ *  根据图片宽高，先对宽高进行缩小，再压缩
+ *
+ *  @param image 原图
+ *
+ *  @return 新图的data
+ */
++ (NSData *)zipNSDataWithImage:(UIImage *)sourceImage;
 @end
--- iOSDemo/Category/UIImage+Extension.m
+++ iOSDemo/Category/UIImage+Extension.m
@@ -62,4 +62,61 @@
     
     return scaledImage;   //返回的就是已经改变的图片
 }
+
+/**
+ *  仿微信对图片进行压缩
+ *  根据图片宽高，先对宽高进行缩小，再压缩
+ *
+ *  @param image 原图
+ *
+ *  @return 新图的data
+ */
++ (NSData *)zipNSDataWithImage:(UIImage *)sourceImage{
+    //进行图像尺寸的压缩
+    CGSize imageSize = sourceImage.size;//取出要压缩的image尺寸
+    CGFloat width = imageSize.width;    //图片宽度
+    CGFloat height = imageSize.height;  //图片高度
+    //1.宽高大于1280(宽高比不按照2来算，按照1来算)
+    if (width>1280||height>1280) {
+        if (width>height) {
+            CGFloat scale = height/width;
+            width = 1280;
+            height = width*scale;
+        }else{
+            CGFloat scale = width/height;
+            height = 1280;
+            width = height*scale;
+        }
+        //2.宽大于1280高小于1280
+    }else if(width>1280||height<1280){
+        CGFloat scale = height/width;
+        width = 1280;
+        height = width*scale;
+        //3.宽小于1280高大于1280
+    }else if(width<1280||height>1280){
+        CGFloat scale = width/height;
+        height = 1280;
+        width = height*scale;
+        //4.宽高都小于1280
+    }else{
+    }
+    UIGraphicsBeginImageContext(CGSizeMake(width, height));
+    [sourceImage drawInRect:CGRectMake(0,0,width,height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    //进行图像的画面质量压缩
+    NSData *data=UIImageJPEGRepresentation(newImage, 1.0);
+    if (data.length>100*1024) {
+        if (data.length>1024*1024) {//1M以及以上
+            data=UIImageJPEGRepresentation(newImage, 0.7);
+        }else if (data.length>512*1024) {//0.5M-1M
+            data=UIImageJPEGRepresentation(newImage, 0.8);
+        }else if (data.length>200*1024) {
+            //0.25M-0.5M
+            data=UIImageJPEGRepresentation(newImage, 0.9);
+        }
+    }
+    return data;
+}
 @end
